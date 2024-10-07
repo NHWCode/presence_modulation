7e57cc7e4800f6bf75218261ecb4639b3ff35db9
Modulate Presence <nipunihw@gmail.com>
2024-10-07 12:05:21 +1100

    Update connection.py

--- context_identification/connection.py
+++ context_identification/connection.py
@@ -1,6 +1,6 @@
 # MIT License
 # 
-# Copyright (c) [2024] [Nipuni Hansika Wijesinghe]
+# Copyright (c) [2024] Modulate Presence
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
